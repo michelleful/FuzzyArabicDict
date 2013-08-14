1859bedfa7260d07d2256d94b1467dcf55113d55
Michelle Fullwood <michelle.fullwood@gmail.com>
2013-08-14 18:09:15 -0400

    Corrected punctuation mistake in settings

--- fuzzyarabicdict/fuzzyarabicdict/settings.py
+++ fuzzyarabicdict/fuzzyarabicdict/settings.py
@@ -125,7 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'south',
-    'django-extensions',
+    'django_extensions',
 )
 
 # A sample logging configuration. The only tangible logging
