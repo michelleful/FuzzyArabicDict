685f245fce53de5b17bef44773e9fdf8317a6cd1
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-03-14 15:21:34 -0400

    More changes to transliteration schema, should be about 95% correct now. Addresses issue #2

--- aramorph/transliterate.py
+++ aramorph/transliterate.py
@@ -9,11 +9,22 @@ unic = u"".join(map(unichr,
              list(range(0x0621, 0x063b)) + # hamza through ghayn
              list(range(0x0640, 0x0653)) + # taTwiil through sukuun
              list(range(0x0660, 0x066A)) + # numerals
-             list(range(0x0670, 0x0672)))) # dagger 'alif, waSla
-ala  = ["ʾ","|","ʾ","ʾ","ʾ","ʾ","ā","b","ah","t","th","j","ḥ","kh",
-        "d","dh","r","z","s","sh","ṣ","ḍ","ṭ","ẓ","ʿ","gh","","f","q","k","l",
-        "m","n","h","w","ā","y","an","un","in","a","u","i","~","",
-        "0","1","2","3","4","5","6","7","8","9","`","{"]
+             list(range(0x0670, 0x0671)) + # dagger 'alif
+             list(range(0x0627, 0x0628)))) # waSla
+             # waSla doesn't seem to appear in many web-friendly fonts
+             # so I'll display an alif instead
+
+# more reader-friendly transliteration - ALA/Wehr 
+ala  = [# hamza through ghayn
+        u"ʾ",u"ʾā",u"ʾ",u"ʾ",u"ʾ",u"ʾ",u"ā",u"b",u"h",u"t",u"ṯ",u"j",u"ḥ",u"ḵ",
+        u"d",u"ḏ",u"r",u"z",u"s",u"š",u"ṣ",u"ḍ",u"ṭ",u"ẓ",u"ʿ",u"ḡ",
+        # taTwiil through sukuun
+        u"",u"f",u"q",u"k",u"l",u"m",u"n",u"h",u"w",u"ā",u"y",
+        u"an",u"un",u"in",u"a",u"u",u"i",u"~",u"",
+        # numerals
+        u"0",u"1",u"2",u"3",u"4",u"5",u"6",u"7",u"8",u"9",
+        # dagger 'alif, waSla
+        u"ā",u""]
 
 assert len(buck) == len(unic) == len(ala)
 
@@ -26,14 +37,25 @@ def translate_b2u(buckwalter_string):
 def translate_u2b(unicode_string):
     return unicode_string.translate(unic2buck)
 
+def translate_b2ala_letter(letter):
+    try:
+        return ala[buck.index(letter)]
+    except:
+        return letter
+
 def translate_b2ala(buckwalter_string):
     # deal with shadda (doubled letters)
     string = re.sub(r"(.)~", r"\1\1", buckwalter_string)
 
-    string = "".join([ala[buck.index(letter)] for letter in string])
-    # deal with madda
-    # TODO
+    string = u"".join([translate_b2ala_letter(letter) for letter in string])
+
+    # deal with uw, iy - make them ū and ī
+    # might not be 100% correct - should they remain "uw", "iy" in some contexts?
+    string = string.replace(u"uw", u"ū")
+    string = string.replace(u"iy", u"ī")
     
-    # deal with uw, iy
+    # deal with repeated vowels    
+    string = string.replace(u"āa", u"ā")
+    string = string.replace(u"ūu", u"ū")
     
     return string
