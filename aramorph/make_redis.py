1a7932d46ecf42db46d4959c57a917bfb06f5415
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-03-17 13:17:46 -0400

    Split make_redis up to separate file processing logic from redis code

--- aramorph/make_redis.py
+++ aramorph/make_redis.py
@@ -1,87 +1,14 @@
-import re
 import redis
 from collections import defaultdict
+from process_files import process_textfile, process_tableXY
 
 r_server = redis.Redis("localhost")
 
 # first remove all keys
 r_server.flushdb()
 
-def process_textfile(filename):
-    with open(filename, "r") as f:
-        for line in f:
-            if line.startswith(';'):
-                continue
-            try:
-                (unvowelled, vowelled, cat, gloss) = line.split('\t')
-                # strip after we've done the split so that the 
-                # zero prefix/suffix is handled correctly
-                yield (unvowelled, vowelled, cat, gloss.strip())
-            except:
-                continue
-
-p_AZ = re.compile('^[A-Z]')
-p_iy = re.compile('iy~$')
-
-pos_replacement = {
-    'ADJ': 'Adjective',
-    'ADV': 'Adverb',
-    'ABBREV': 'Abbreviation',
-    'PREP': 'Preposition',
-    'NEG_PART': 'Negative particle',
-    'CONJ': 'Conjunction',
-    'INTERJ': 'Interjection',
-    'NOUN_PROP': 'Proper noun',
-    'FUNC_WORD': 'Function word',
-    'REL_PRON': 'Relative pronoun',
-    'DET': 'Determiner',
-    'DEM_PRON': 'Demonstrative pronoun',
-    'INTERROG_PART': 'Interrogative pronoun',
-    'FUT_PART': 'Future particle'
-}
-
-def process_pos(voc, cat, glossPOS):
-    m = re.search('<pos>.*/(.+?)</pos>', glossPOS)
-    if m:
-        POS = m.group(1)
-        gloss = glossPOS
-        # replace abbreviations as above with their long forms
-        for pos_type, long_pos_type in pos_replacement.items():
-            if pos_type.startswith(POS):
-                POS = long_pos_type
-    else:
-        gloss = glossPOS
-        if cat.startswith('Pref-0') or cat.startswith('Suff-0'):
-            POS = "" # null prefix or suffix
-        elif cat.startswith('F'):
-            POS = "Function word"
-        elif cat.startswith('IV'):
-            POS = "Imperfect verb"
-        elif cat.startswith('PV'):
-            POS = "Perfect verb"
-        elif cat.startswith('CV'):
-            POS = "Imperative verb"
-        elif cat.startswith('N') and p_AZ.search(gloss):
-            POS = "Proper noun" # educated guess, (99% correct)
-        elif cat.startswith('N') and p_iy.search(voc):
-            POS = "Noun" # (was NOUN_ADJ: some of these are really ADJ' and need to be tagged manually)
-        elif cat.startswith('N'):
-            POS = "Noun"
-        elif POS.startswith('ADJ'):
-            POS = "Adjective"
-        else:
-            print "no POS can be deduced for %s" % voc
-            assert False
-
-    # make POS sentence case
-    POS = POS.capitalize()
-
-    gloss = re.sub('<pos>.+?</pos>', '', gloss)
-    gloss = gloss.strip()
-    return POS, gloss
-
 def add_morpheme_to_redis(morpheme_type, morpheme_dict, 
-                          unvowelled, vowelled, cat, gloss, root=None):
+                          unvowelled, vowelled, cat, pos, gloss, root=None):
     if morpheme_type.endswith('x'):
         plural = morpheme_type + 'es'
     else:
@@ -89,9 +16,6 @@ def add_morpheme_to_redis(morpheme_type, morpheme_dict,
 
     # add morpheme to set of morphemes "prefixes", "stems" or "suffixes"
     r_server.sadd(plural, unvowelled)
-
-    # make pos and gloss more human-readable
-    pos, gloss = process_pos(vowelled, cat, gloss)
 
     # there could be multiple morphemes with the same unvowelled form
     # so create a set of morpheme types with this unvowelled form
@@ -107,34 +31,29 @@ def add_morpheme_to_redis(morpheme_type, morpheme_dict,
                      "gloss": gloss})
     if root:
         r_server.hset("%s:%s:%s" % (morpheme_))
-    
+
+def process_morpheme_file_for_redis(filename, morpheme_type, morpheme_dict):
+    for (unvowelled, vowelled, cat, pos, gloss) in process_textfile(filename):
+        add_morpheme_to_redis(morpheme_type, morpheme_dict, 
+                              unvowelled, vowelled, cat, pos, gloss)
+
+def process_tableXY_for_redis(filename):
+    for (left, right) in process_tableXY(filename):
+        r_server.sadd(filename, " ".join([left, right]))
+
+# -------------
+#    MAIN
+# -------------
 prefix_dict = defaultdict(int)
-def process_prefixes():
-    for (unvowelled, vowelled, cat, gloss) in process_textfile("dictprefixes"):    
-        add_morpheme_to_redis("prefix", prefix_dict, unvowelled, vowelled, cat, gloss)
-
-suffix_dict = defaultdict(int)
-def process_suffixes():
-    for (unvowelled, vowelled, cat, gloss) in process_textfile("dictsuffixes"):    
-        add_morpheme_to_redis("suffix", suffix_dict, unvowelled, vowelled, cat, gloss)
-
 stem_dict = defaultdict(int)
-def process_stems():
-    for (unvowelled, vowelled, cat, gloss) in process_textfile("dictstems"):    
-        add_morpheme_to_redis("stem", stem_dict, unvowelled, vowelled, cat, gloss)
+suffix_dict = defaultdict(int)
 
-def process_tableXY(filename):
-    with open(filename, "r") as f:
-        for line in f:
-            if line.startswith(';'):
-                continue
-            r_server.sadd(filename, line.strip())
+process_morpheme_file_for_redis("dictprefixes", "prefix", prefix_dict)
+process_morpheme_file_for_redis("dictstems", "stem", stem_dict)
+process_morpheme_file_for_redis("dictsuffixes", "suffix", suffix_dict)
 
-process_prefixes()
-process_stems()
-process_suffixes()
-process_tableXY("tableab")
-process_tableXY("tablebc")
-process_tableXY("tableac")
+process_tableXY_for_redis("tableab")
+process_tableXY_for_redis("tablebc")
+process_tableXY_for_redis("tableac")
 
 
---
+++ aramorph/process_files.py
@@ -0,0 +1,85 @@
+import re
+
+def process_textfile(filename):
+    with open(filename, "r") as f:
+        for line in f:
+            if line.startswith(';'):
+                continue
+            try:
+                (unvowelled, vowelled, cat, gloss) = line.split('\t')
+                # strip after we've done the split so that the 
+                # zero prefix/suffix is handled correctly
+
+                # make pos and gloss more human-readable
+                pos, gloss = process_pos(vowelled, cat, gloss)
+                yield (unvowelled, vowelled, cat, pos, gloss)
+            except:
+                continue
+
+p_AZ = re.compile('^[A-Z]')
+p_iy = re.compile('iy~$')
+
+pos_replacement = {
+    'ADJ': 'Adjective',
+    'ADV': 'Adverb',
+    'ABBREV': 'Abbreviation',
+    'PREP': 'Preposition',
+    'NEG_PART': 'Negative particle',
+    'CONJ': 'Conjunction',
+    'INTERJ': 'Interjection',
+    'NOUN_PROP': 'Proper noun',
+    'FUNC_WORD': 'Function word',
+    'REL_PRON': 'Relative pronoun',
+    'DET': 'Determiner',
+    'DEM_PRON': 'Demonstrative pronoun',
+    'INTERROG_PART': 'Interrogative pronoun',
+    'FUT_PART': 'Future particle'
+}
+
+def process_pos(voc, cat, glossPOS):
+    m = re.search('<pos>.*/(.+?)</pos>', glossPOS)
+    if m:
+        POS = m.group(1)
+        gloss = glossPOS
+        # replace abbreviations as above with their long forms
+        for pos_type, long_pos_type in pos_replacement.items():
+            if pos_type.startswith(POS):
+                POS = long_pos_type
+    else:
+        gloss = glossPOS
+        if cat.startswith('Pref-0') or cat.startswith('Suff-0'):
+            POS = "" # null prefix or suffix
+        elif cat.startswith('F'):
+            POS = "Function word"
+        elif cat.startswith('IV'):
+            POS = "Imperfect verb"
+        elif cat.startswith('PV'):
+            POS = "Perfect verb"
+        elif cat.startswith('CV'):
+            POS = "Imperative verb"
+        elif cat.startswith('N') and p_AZ.search(gloss):
+            POS = "Proper noun" # educated guess, (99% correct)
+        elif cat.startswith('N') and p_iy.search(voc):
+            POS = "Noun" # (was NOUN_ADJ: some of these are really ADJ' and need to be tagged manually)
+        elif cat.startswith('N'):
+            POS = "Noun"
+        elif POS.startswith('ADJ'):
+            POS = "Adjective"
+        else:
+            print "no POS can be deduced for %s" % voc
+            assert False
+
+    # make POS sentence case
+    POS = POS.capitalize()
+
+    gloss = re.sub('<pos>.+?</pos>', '', gloss)
+    gloss = gloss.strip()
+    return POS, gloss
+
+def process_tableXY(filename):
+    with open(filename, "r") as f:
+        for line in f:
+            if line.startswith(';'):
+                continue
+            yield line.strip().split()
+
