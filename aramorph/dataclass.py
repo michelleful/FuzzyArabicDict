dd3b4fd0677bbb4bffef09157489985889bb96fc
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-03-17 20:41:34 -0400

    Fixed search/replace bug in dataclass

--- aramorph/dataclass.py
+++ aramorph/dataclass.py
@@ -18,20 +18,20 @@ class AramorphInfo(object):
         self.ac = ac
         
     def is_valid_prefix(self, unvowelled_prefix):
-        return unvowelled_prefix in ai.prefixes
+        return unvowelled_prefix in self.prefixes
         
     def is_valid_stem(self, unvowelled_stem):
-        return unvowelled_stem in ai.stems
+        return unvowelled_stem in self.stems
 
     def is_valid_suffix(self, unvowelled_suffix):
-        return unvowelled_suffix in ai.suffixes
+        return unvowelled_suffix in self.suffixes
         
     def are_prefix_stem_compatible(self, prefix_morpheme, stem_morpheme):
-        return stem_morpheme.cat in ai.ab[prefix_morpheme.cat]
+        return stem_morpheme.cat in self.ab[prefix_morpheme.cat]
         
     def are_stem_suffix_compatible(self, stem_morpheme, suffix_morpheme):
-        return suffix_morpheme.cat in ai.bc[stem_morpheme.cat]
+        return suffix_morpheme.cat in self.bc[stem_morpheme.cat]
 
     def are_prefix_suffix_compatible(self, prefix_morpheme, suffix_morpheme):
-        return suffix_morpheme.cat in ai.ac[prefix_morpheme.cat]
+        return suffix_morpheme.cat in self.ac[prefix_morpheme.cat]
 
