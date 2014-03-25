9b092967c145d60ccb49ec45ef93f0226ccd93f8
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-03-25 11:28:02 -0400

    Added caching

--- fuzzy.py
+++ fuzzy.py
@@ -2,12 +2,13 @@
 
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, jsonify
-
+from flask.ext.cacheify import init_cacheify
 from aramorph import analyser
 
 # create the application
 app = Flask(__name__)
 app.config.from_object(__name__)
+cache = init_cacheify(app)
 
 # not using a database
 # not sure if I need this line:
@@ -22,24 +23,20 @@ def main():
 @app.route('/query_ajax', methods=['POST'])
 def query():
     if request.json and 'words' in request.json:
-        results = list()
-        words = request.json['words'].split(",")
-        for word in words:
-            for analysis in analyser.ai.analyse_arabic(word):
-                if analysis and analysis not in results:
-                    results.append(analysis)
-        if len(results) > 0:
-            return jsonify(analyses=results), 201
-        else:
-            # Yamli gave us some words but we didn't find them in Buckwalter AMA
-            # give some abbreviated information anyway
-            results = analyser.ai.information(words)
-            return jsonify(analyses=results), 201
-
-# TODO: add route like /kitab etc
-# fuzzy.herokuapp.com/kitab ---> page with "kitab" already filled in and all the possibilities displayed
-#def word():
-#    return render_template('yamli.html', {'word': word})
+        results = cache.get(request.json['words'])
+        if results is None:
+            results = list()
+            words = request.json['words'].split(",")
+            for word in words:
+                for analysis in analyser.ai.analyse_arabic(word):
+                    if analysis and analysis not in results:
+                        results.append(analysis)
+            if len(results) == 0:
+                # Yamli gave us some words but we didn't find them in Buckwalter AMA
+                # give some abbreviated information anyway
+                results = analyser.ai.information(words)
+            cache.set(request.json['words'], results)
+        return jsonify(analyses=results), 201
 
 if __name__ == '__main__':
     app.run(debug=False)
